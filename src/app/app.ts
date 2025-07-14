4a526de647e73a3b4b8834fc422a07379fb56369
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-14 14:09:01 +0300

    update: display main and aside

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Aside } from './components/aside/aside';
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Aside],
+  imports: [Header, Aside, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
